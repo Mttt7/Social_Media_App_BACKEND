838f20755c9c2d764e0963be4801460ab8adf25a
Michal Tomaszewski <groobytt@gmail.com>
2024-01-29 09:50:16 +0100

    add get All Posts by User ID endpoint

--- src/main/java/com/mt/mtSocialMedia/repository/PostRepository.java
+++ src/main/java/com/mt/mtSocialMedia/repository/PostRepository.java
@@ -1,7 +1,13 @@
 package com.mt.mtSocialMedia.repository;
 
 import com.mt.mtSocialMedia.model.Post;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface PostRepository extends JpaRepository<Post,Long> {
+
+    Page<Post> findAllByUserEntity_Id(Long id, PageRequest pg);
 }
